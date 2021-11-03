972dcab9525e60d59f8110e6e6f1d3f42b5a81c6
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2021-11-03 18:45:38 +0100

    lo78

--- sandbox/lo78_01.py
+++ sandbox/lo78_01.py
@@ -18,8 +18,8 @@ circle_area = pi * circle_radius ** 2
 print(f"Area is {circle_area}")
 
 # wypisujemy informacje o typie danych:
-print(f"Typ zmiennej language to {type(language)}".")
-print(f"Typ zmiennej year_of_birth to {type(year_of_birth)}".")
-print(f"Typ zmiennej version to {type(version)}".")
-print(f"Typ zmiennej is_cool to {type(is_cool)}".")
-print(f"Typ zmiennej circle_area to {type(circle_area)}".")
+print(f"Typ zmiennej language to {type(language)}.")
+print(f"Typ zmiennej year_of_birth to {type(year_of_birth)}.")
+print(f"Typ zmiennej version to {type(version)}.")
+print(f"Typ zmiennej is_cool to {type(is_cool)}.")
+print(f"Typ zmiennej circle_area to {type(circle_area)}.")
